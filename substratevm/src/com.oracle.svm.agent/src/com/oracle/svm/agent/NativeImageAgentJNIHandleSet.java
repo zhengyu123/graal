915e8b0d325afbb2c086a79c666431c600f0edce
Loic Ottet <loic.ottet@oracle.com>
2021-06-08 16:34:57 +0200

    Fix ConstantBootstraps agent breakpoints

--- substratevm/src/com.oracle.svm.agent/src/com/oracle/svm/agent/NativeImageAgentJNIHandleSet.java
+++ substratevm/src/com.oracle.svm.agent/src/com/oracle/svm/agent/NativeImageAgentJNIHandleSet.java
@@ -55,6 +55,7 @@ public class NativeImageAgentJNIHandleSet extends JNIHandleSet {
     private JNIMethodId javaLangInvokeMethodTypeParameterArray = WordFactory.nullPointer();
     private JNIMethodId javaLangInvokeMethodTypeReturnType = WordFactory.nullPointer();
     final JNIObjectHandle javaLangIllegalAccessException;
+    final JNIObjectHandle javaLangIllegalAccessError;
     final JNIObjectHandle javaLangInvokeWrongMethodTypeException;
     final JNIObjectHandle javaLangIllegalArgumentException;
 
@@ -95,6 +96,7 @@ public class NativeImageAgentJNIHandleSet extends JNIHandleSet {
         javaLangObjectGetClass = getMethodId(env, javaLangObject, "getClass", "()Ljava/lang/Class;", false);
 
         javaLangIllegalAccessException = newClassGlobalRef(env, "java/lang/IllegalAccessException");
+        javaLangIllegalAccessError = newClassGlobalRef(env, "java/lang/IllegalAccessError");
         javaLangInvokeWrongMethodTypeException = newClassGlobalRef(env, "java/lang/invoke/WrongMethodTypeException");
         javaLangIllegalArgumentException = newClassGlobalRef(env, "java/lang/IllegalArgumentException");
     }

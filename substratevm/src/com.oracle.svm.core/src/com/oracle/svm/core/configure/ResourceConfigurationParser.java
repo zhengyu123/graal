Wesley-yang/graal-1

Relax the configuration format and report unknown attributes once per configuration object.

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/configure/ResourceConfigurationParser.java
@@ -26,12 +26,12 @@ package com.oracle.svm.core.configure;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
 import com.oracle.svm.core.util.json.JSONParser;
-import com.oracle.svm.core.util.json.JSONParserException;
 
 public class ResourceConfigurationParser extends ConfigurationParser {
     private final ResourcesRegistry registry;
@@ -60,15 +60,10 @@ public class ResourceConfigurationParser extends ConfigurationParser {
         }
         if (resourcesObject != null) {
             if (resourcesObject instanceof Map) { // New format
-                Object includesObject = null;
-                Object excludesObject = null;
-                for (Map.Entry<String, Object> pair : ((Map<String, Object>) resourcesObject).entrySet()) {
-                    if ("includes".equals(pair.getKey())) {
-                        includesObject = pair.getValue();
-                    } else if ("excludes".equals(pair.getKey())) {
-                        excludesObject = pair.getValue();
-                    }
-                }
+                Map<String, Object> resourcesObjectMap = (Map<String, Object>) resourcesObject;
+                checkAttributes(resourcesObjectMap, "resource descriptor object", Collections.singleton("includes"), Collections.singleton("excludes"));
+                Object includesObject = resourcesObjectMap.get("includes");
+                Object excludesObject = resourcesObjectMap.get("excludes");
 
                 List<Object> includes = asList(includesObject, "Attribute 'includes' must be a list of resources");
                 for (Object object : includes) {
@@ -96,17 +91,10 @@ public class ResourceConfigurationParser extends ConfigurationParser {
         }
     }
 
-    private static void parseEntry(Object data, String valueKey, Consumer<String> registry, String expectedType, String parentType) {
+    private void parseEntry(Object data, String valueKey, Consumer<String> registry, String expectedType, String parentType) {
         Map<String, Object> resource = asMap(data, "Elements of " + parentType + " must be a " + expectedType);
-        Object valueObject = null;
-        for (Map.Entry<String, Object> pair : resource.entrySet()) {
-            if (valueKey.equals(pair.getKey())) {
-                valueObject = pair.getValue();
-            }
-        }
-        if (valueObject == null) {
-            throw new JSONParserException("Missing attribute '" + valueKey + "' in " + expectedType);
-        }
+        checkAttributes(resource, "resource and resource bundle descriptor object", Collections.singleton(valueKey));
+        Object valueObject = resource.get(valueKey);
         String value = asString(valueObject, valueKey);
         registry.accept(value);
     }

zhengyu123/graal

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/configure/ResourceConfigurationParser.java
@@ -26,12 +26,12 @@ package com.oracle.svm.core.configure;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
 import com.oracle.svm.core.util.json.JSONParser;
-import com.oracle.svm.core.util.json.JSONParserException;
 
 public class ResourceConfigurationParser extends ConfigurationParser {
     private final ResourcesRegistry registry;
@@ -60,15 +60,10 @@ public class ResourceConfigurationParser extends ConfigurationParser {
         }
         if (resourcesObject != null) {
             if (resourcesObject instanceof Map) { // New format
-                Object includesObject = null;
-                Object excludesObject = null;
-                for (Map.Entry<String, Object> pair : ((Map<String, Object>) resourcesObject).entrySet()) {
-                    if ("includes".equals(pair.getKey())) {
-                        includesObject = pair.getValue();
-                    } else if ("excludes".equals(pair.getKey())) {
-                        excludesObject = pair.getValue();
-                    }
-                }
+                Map<String, Object> resourcesObjectMap = (Map<String, Object>) resourcesObject;
+                checkAttributes(resourcesObjectMap, "resource descriptor object", Collections.singleton("includes"), Collections.singleton("excludes"));
+                Object includesObject = resourcesObjectMap.get("includes");
+                Object excludesObject = resourcesObjectMap.get("excludes");
 
                 List<Object> includes = asList(includesObject, "Attribute 'includes' must be a list of resources");
                 for (Object object : includes) {
@@ -96,18 +91,11 @@ public class ResourceConfigurationParser extends ConfigurationParser {
         }
     }
 
-    private static void parseEntry(Object data, String valueKey, Consumer<String> registry, String expectedType, String parentType) {
+    private void parseEntry(Object data, String valueKey, Consumer<String> resourceRegistry, String expectedType, String parentType) {
         Map<String, Object> resource = asMap(data, "Elements of " + parentType + " must be a " + expectedType);
-        Object valueObject = null;
-        for (Map.Entry<String, Object> pair : resource.entrySet()) {
-            if (valueKey.equals(pair.getKey())) {
-                valueObject = pair.getValue();
-            }
-        }
-        if (valueObject == null) {
-            throw new JSONParserException("Missing attribute '" + valueKey + "' in " + expectedType);
-        }
+        checkAttributes(resource, "resource and resource bundle descriptor object", Collections.singleton(valueKey));
+        Object valueObject = resource.get(valueKey);
         String value = asString(valueObject, valueKey);
-        registry.accept(value);
+        resourceRegistry.accept(value);
     }
 }